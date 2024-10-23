2073c612904eb72b2eac5613af0b7e6063d723d2
Reywaltz <george1147@yandex.ru>
2024-10-23 05:26:14 +0300

    added db migrations

--- internal/repositories/user_repo.go
+++ internal/repositories/user_repo.go
@@ -5,27 +5,15 @@ import (
 	db "eattogether/pkg/db"
 
 	"eattogether/internal/models"
-
-	"github.com/jackc/pgx/v5"
-	pgconn "github.com/jackc/pgx/v5/pgconn"
 )
 
-type PgxInterface interface {
-	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Row, error)
-	Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error)
-}
-
 type UserRepo struct {
-	DB PgxInterface
+	DB db.PgxInterface
 }
 
 func (p *UserRepo) GetUser() (models.User, error) {
 	var user models.User
-	res, err := p.DB.Query(context.Background(), "SELECT * FROM places")
-	if err != nil {
-		return user, err
-	}
+	res := p.DB.QueryRow(context.Background(), "SELECT * FROM places")
 
 	res.Scan(&user)
 	return user, nil
--- internal/services/place_service.go
+++ internal/services/place_service.go
@@ -4,6 +4,7 @@ import (
 	"eattogether/internal/models"
 	"eattogether/internal/repositories"
 	env "eattogether/pkg/env"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,15 +18,12 @@ type PlaceService struct {
 func (p *PlaceService) GetPlaces(c echo.Context) error {
 	var response models.PlacePayload
 
-	items := make([]models.Place, 10)
-
-	for id := range items {
-		items[id].ID = id + 1
-		items[id].Name = "Галки"
-		items[id].Image = "/Path/to/image"
+	places, err := p.PlaceRepo.GetPlaces()
+	if err != nil {
+		fmt.Printf("Can't get data from db:%v\n", err)
 	}
 
-	response.Items = items
+	response.Items = places
 
 	c.JSON(http.StatusOK, response)
 	return nil
